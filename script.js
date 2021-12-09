05a92c23fc5ebe68b598a74f6bef7a81297254be
Davit <90.davitmanukyan@mail.ru>
2021-12-09 20:19:19 +0400

    Type Coercion for primitives

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+let str1 = "" + 1 + 0;
+console.log("10");
+
+let str2 = "" - 1 + 0;
+console.log("-1");
+
+let str3 = true + false;
+console.log("1");
+
+let str4 = 6 / "3";
+console.log("2");
+
+let str5 = "2" * "3";
+console.log("6");
+
+let str6 = 4 + 5 + "px";
+console.log("9px");
+
+let str7 = "$" + 4 + 5;
+console.log("$45");
+
+let str8 = "4" - 2;
+console.log("2");
+
+let str9 = "4px" - 2;
+console.log("NaN");
+
+let str10 = 7 / 0;
+console.log("Infinity");
+
+let str11 = " -9 " + 5;
+console.log("-95");
+
+let str12 = null + 1;
+console.log("1");
+
+let str13 = undefined + 1;
+console.log("NaN");
